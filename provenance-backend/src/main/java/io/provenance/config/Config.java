b965608865d1d49ae10c4b5e197d7804590f0f71
Gerrit Janßen <gerrit.janssen@mailbox.tu-berlin.de>
2018-02-07 18:24:55 +0100

    config setup by env variables

--- provenance-backend/src/main/java/io/provenance/config/Config.java
+++ provenance-backend/src/main/java/io/provenance/config/Config.java
@@ -1,11 +1,12 @@
 package io.provenance.config;
 
+import io.provenance.exception.ConfigParseException;
+
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
-import io.provenance.exception.ConfigParseException;
 
 public class Config {
 
@@ -34,13 +35,32 @@ public class Config {
 		InputStream inputStream = null;
 		Properties properties = new Properties();
 		try {
-			inputStream = new FileInputStream(configPath);
-			properties.loadFromXML(inputStream);
-			DB_IP = properties.getProperty("DB_IP");
-			DB_PORT = Integer.parseInt(properties.getProperty("DB_PORT"));
-			KEYSPACE = properties.getProperty("KEYSPACE");
-			TABLE = properties.getProperty("TABLE");
-			inputStream.close();
+			if(System.getenv("CONF_LOC") != null && System.getenv("CONF_LOC").toUpperCase().equals("EVAR")){
+				if(System.getenv("DB_IP") != null){
+					DB_IP = System.getenv("DB_IP");
+				}
+
+				if(System.getenv("DB_PORT") != null){
+					DB_PORT = Integer.parseInt(System.getenv("DB_PORT"));
+				}
+
+				if(System.getenv("KEYSPACE") != null){
+					KEYSPACE = System.getenv("KEYSPACE");
+				}
+
+				if(System.getenv("TABLE") != null){
+					TABLE = System.getenv("TABLE");
+				}
+			}
+			else {
+				inputStream = new FileInputStream(configPath);
+				properties.loadFromXML(inputStream);
+				DB_IP = properties.getProperty("DB_IP");
+				DB_PORT = Integer.parseInt(properties.getProperty("DB_PORT"));
+				KEYSPACE = properties.getProperty("KEYSPACE");
+				TABLE = properties.getProperty("TABLE");
+				inputStream.close();
+			}
 		} catch(FileNotFoundException fnex) {
 			throw new ConfigParseException("Problem in loading configuration file.");
 		} catch(IOException ioex) {
